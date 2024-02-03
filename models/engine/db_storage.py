ee1dcf768d48acdb441d241edef3fee1c7569f17
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-02-03 13:45:06 +0100

    Setting up for DBStorage - Added the connection string to an sqlite DB storage - Added the various methods for the DB storage - Added two lines in .gitignore to prevent venv from being sent to the repo - Added a mock storage in the test_base_model when testing the save method of the BaseModel

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,8 +3,10 @@
 Contains the class DBStorage
 """
 import models
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from models.user import User
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 class DBStorage:
@@ -16,31 +18,41 @@ class DBStorage:
 
     def __init__(self):
         """Initializes the DB storage class"""
-        pass
+        self.__engine = create_engine("sqlite:///wellnourish.db")
 
     def all(self, cls=None):
         """Returns object dictionary of the data in database"""
-        pass
+        all_dict = {}
+        if cls is None or cls is User:
+            objs = self.__session.query(cls).all()
+            for obj in objs:
+                key = obj.__class__.__name__ + '.' + obj.id
+                all_dict[key] = obj
+        return (all_dict)
 
     def new(self, obj):
         """Add the object to the current database session"""
-        pass
+        self.__session.add(obj)
 
     def save(self):
         """Commit all changes of the current database session"""
-        pass
+        self.__session.commit()
 
     def delete(self, obj=None):
         """Delete from the current database session obj if not None"""
-        pass
+        if obj is not None:
+            self.__session.delete(obj)
 
     def reload(self):
         """Reloads data from the database"""
-        pass
+        Base.metadata.create_all(self.__engine)
+        factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(factory)
+        self.__session = Session
 
     def close(self):
         """Call remove() method on the private session attribute"""
-        pass
+        self.__session.remove()
 
     def get(self, cls, id):
         """
