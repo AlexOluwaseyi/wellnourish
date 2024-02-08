1e69b187f4edef6c66a5bca7d59e0454295ddeb5
Jesufemi Omitogun <omitogunjesufemi@gmail.com>
2024-02-08 13:06:39 +0100

    APIs for recipes Added api for getting random recipes which can be used on the index page Added api for getting recipes by ingredients Added api for getting more infos as regards the recipes

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,3 +7,4 @@ from flask import Blueprint
 api_views = Blueprint("api_views", __name__, url_prefix='/api/v1')
 
 from api.v1.views.users import *
+from api.v1.views.recipes import *
---
+++ api/v1/views/recipes.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""
+Recipe API Routes
+"""
+from api.v1.views import api_views
+from flask import jsonify, abort, make_response, request
+import requests
+
+
+@api_views.route('/recipes', methods=['GET'], strict_slashes=False)
+def get_random_recipes():
+    """
+    Retrieves a random list of recipes
+    """
+    url = f"https://api.spoonacular.com/recipes/random"
+
+    params = {
+        "apiKey": "c238bda8889a4a9d8e203d71a0f67219",
+        "number": 12
+    }
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+    }
+
+    try:
+        response = requests.get(url, params=params, headers=headers)
+        if response.status_code == 200:
+            recipes = response.json()
+            return recipes
+    except requests.exceptions.RequestException:
+        return None
+
+
+@api_views.route('/recipes/find_by_ingr/<ingredients>', methods=['GET'], strict_slashes=False)
+def get_recipes_by_ingredient(ingredients):
+    """
+    Retrieves a random list of recipes
+    """
+    url = f"https://api.spoonacular.com/recipes/findByIngredients"
+
+    params = {
+        "apiKey": "c238bda8889a4a9d8e203d71a0f67219",
+        "ingredients": ingredients
+    }
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+    }
+
+    try:
+        response = requests.get(url, params=params, headers=headers)
+        if response.status_code == 200:
+            recipes = response.json()
+            return recipes
+    except requests.exceptions.RequestException:
+        return None
+
+
+@api_views.route('/recipes/<recipe_id>', methods=['GET'], strict_slashes=False)
+def get_recipes_by_id(recipe_id):
+    """
+    Retrieves a random list of recipes
+    """
+    url = f"https://api.spoonacular.com/recipes/{recipe_id}/information"
+
+    params = {
+        "apiKey": "c238bda8889a4a9d8e203d71a0f67219"
+    }
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+    }
+
+    try:
+        response = requests.get(url, params=params, headers=headers)
+        if response.status_code == 200:
+            recipes = response.json()
+            return recipes
+    except requests.exceptions.RequestException:
+        return None
